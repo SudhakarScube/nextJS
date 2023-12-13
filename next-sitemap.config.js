30a4f1d5e1506ba7104082987fde07eada5cddde
Sudhakar S <sudhakar@thescube.com>
2023-12-13 18:01:38 +0530

    sitemap removed

--- next-sitemap.config.js
+++
@@ -1,5 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: process.env.SITE_URL || "https://orbitfin-next-js.vercel.app",
-  generateRobotsTxt: false, // (optional)
-};
